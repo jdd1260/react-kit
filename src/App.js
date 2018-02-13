3baadb7974fae58a924956d119cda16c129eb5c2
Joel Detweiler <joel.detweiler@gmail.com>
2018-02-13 13:13:17 +0000

    Add bootstrap and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { Fade } from 'reactstrap';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App text-center">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload sdf.
-        </p>
+        <Fade timeout={ 3000 } >
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </Fade>
       </div>
     );
   }
