f329162b634a0f43bc7a9f10a3236143db586044
Joel Detweiler <joel.detweiler@gmail.com>
2018-02-13 21:00:56 +0000

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
 import './index.css';
+import reducers from './reducers';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleWareStore = applyMiddleware(promise)(createStore);
+
+ReactDOM.render((
+    <Provider store={ middleWareStore(reducers) }>
+        <App />
+    </Provider>
+  ), document.getElementById('root'));
 registerServiceWorker();
